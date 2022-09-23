cee16425b8818bb21f4a781ac85927b60cff93c7
aziz <erazizeee@gmail.com>
2022-09-23 12:46:10 +0530

    review

---
+++ main.js
@@ -0,0 +1,44 @@
+//FOR
+
+// let text = "";
+
+// for (let i = 0; i < 5; i++) {
+//   text += "" + i + "<br>";
+// }
+
+// document.querySelector("#do").innerHTML = text;
+
+//WHILE LOOP
+
+// let b = prompt("Enter the number")
+// let sum = 0;
+// let rem;
+// while (b > 0) {
+//   rem = b % 10;
+//   sum = sum + rem;
+//   b = Math.floor(b / 10);
+// }
+// console.log(sum);
+
+//IF 
+
+// var number = prompt('Enter the number')
+
+// if (number % 9 == 0)
+//   document.write('Multiple of 9')
+// else
+//   document.write('Not multiple of 9')
+
+
+
+//fibonaci
+
+
+var i;
+var x=1;
+for(i=1;i<=10;i++)
+{
+    x*=i;
+}
+console.log("factorial no:",x)
+
